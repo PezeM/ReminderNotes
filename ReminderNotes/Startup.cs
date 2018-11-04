6de6090bfc5709d08da01a502b0e57eb3d4b8a04
PezeM <pezemos@gmail.com>
2018-11-04 16:02:49 +0100

    First commit

--- ReminderNotes/Startup.cs
+++ ReminderNotes/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -49,7 +44,10 @@ namespace ReminderNotes
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+            app.UseNodeModules(env.ContentRootPath);
+
             app.UseCookiePolicy();
 
             app.UseMvc();
