f6b177ca453a8dca3acb4ef2f3cce8faca438b25
PezeM <pezemos@gmail.com>
2018-11-10 12:53:30 +0100

    Added Notes model and updated database.

---
+++ ReminderNotes/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReminderNotes.Models
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+
+        // User ID from AspNetUsers table
+        public string OwnerId { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime ExpireTime { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
