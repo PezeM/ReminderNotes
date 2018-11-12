a2f15ca0dd747cdfccc7abf1c9b59e9607a21b44
PezeM <pezemos@gmail.com>
2018-11-12 17:13:32 +0100

    Notes page now shows all avaible notes for user.

--- ReminderNotes/Pages/Notes/Notes.cshtml.cs
+++ ReminderNotes/Pages/Notes/Notes.cshtml.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ReminderNotes.Models;
+using ReminderNotes.Pages.Notes;
+using ReminderNotes.Services;
 
 namespace ReminderNotes.Pages
 {
-    public class NotesModel : PageModel
+    public class NotesModel : NotesBasePageModel
     {
-        public void OnGet()
-        {
+        public IList<Note> Notes { get; set; }
 
+        public NotesModel(INotesData context,
+            IAuthorizationService authorizationService,
+            UserManager<ReminderNotesUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public async Task OnGetAsync()
+        {
+            var notes = Context.GetAll();
+
+            var currentUserId = UserManager.GetUserId(User);
+
+            // Get all notes created by this user
+            notes = notes.Where(n => n.OwnerId == currentUserId);
+
+            Notes = notes.ToList();
         }
     }
 }
